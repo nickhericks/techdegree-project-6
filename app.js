5d7a8c7edf84153c2093d5660470de998f6da950
Nick-Hericks <nickhericks@gmail.com>
2019-04-10 13:49:47 +0200

    Require data

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
 
+const projects = require('./data.json').projects;
+console.log(projects[0].id);
+
+
 
 app.get('/', (req, res) => {
 	res.render('index');
