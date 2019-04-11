86a4a1bcfd7d7b440effbd25d78227820cf03bf5
Nick-Hericks <nickhericks@gmail.com>
2019-04-11 19:17:36 +0200

    Update error template

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
 	res.locals.error = err;
 	res.status(err.status);
+	// console.log('/error/error');
 	res.render('error');
 });
 
